cd9313afecd2bf86e021847dc66519bf28134656
Bishal Joshi <bishal.joshi10@gmail.com>
2025-06-30 16:52:04 +0530

    swagger installation & setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Payments & Accounts Management System')
+    .setDescription(
+      'System that helps manage payment transactions and account information',
+    )
+    .setVersion('1.0')
+    .addTag('pams')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
