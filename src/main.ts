e3fd47a5b17af6e58942d9f770b6005ba2c72e34
Bishal Joshi <bishal.joshi10@gmail.com>
2025-06-30 22:42:12 +0530

    Rollback: swagger installation & setup

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // swagger setup
-  const config = new DocumentBuilder()
-    .setTitle('Payments & Accounts Management System')
-    .setDescription(
-      'System that helps manage payment transactions and account information',
-    )
-    .setVersion('1.0')
-    .addTag('pams')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe());
 
